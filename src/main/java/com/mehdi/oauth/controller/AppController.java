e9c1b6919513b1ac0cf121c686853c7545a82f44
Mehdi BENFREDJ <mehdibenfredj3@gmail.com>
2024-12-08 17:10:56 +0100

    refactor

--- src/main/java/com/mehdi/oauth/controller/AppController.java
+++ src/main/java/com/mehdi/oauth/controller/AppController.java
@@ -2,9 +2,7 @@ package com.mehdi.oauth.controller;
 
 import jakarta.servlet.Filter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +10,6 @@ import java.util.Map;
 
 @RestController
 public class AppController {
-
-    private final Filter springSecurityFilterChain;
-
-    public AppController(Filter springSecurityFilterChain) {
-        this.springSecurityFilterChain = springSecurityFilterChain;
-    }
 
     @GetMapping("/public")
     public String publicPage() {
--- src/main/java/com/mehdi/oauth/security/OAuth2SuccessHandler.java
+++ src/main/java/com/mehdi/oauth/security/OAuth2SuccessHandler.java
@@ -3,7 +3,6 @@ package com.mehdi.oauth.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
