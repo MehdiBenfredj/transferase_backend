0173ea330900820076ac58e240533b4e36df1c17
Mehdi BENFREDJ <mehdibenfredj3@gmail.com>
2024-12-08 17:11:27 +0100

    refactor

--- src/main/java/com/mehdi/oauth/controller/AppController.java
+++ src/main/java/com/mehdi/oauth/controller/AppController.java
@@ -1,12 +1,10 @@
 package com.mehdi.oauth.controller;
 
-import jakarta.servlet.Filter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
 public class AppController {
