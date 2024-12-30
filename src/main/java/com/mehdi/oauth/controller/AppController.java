77c51c7a3259c4c978853036e5d5e9114c126708
Mehdi BENFREDJ <mehdibenfredj3@gmail.com>
2024-12-30 22:11:36 +0100

    music api token generation

--- src/main/java/com/mehdi/oauth/controller/AppController.java
+++ src/main/java/com/mehdi/oauth/controller/AppController.java
@@ -1,5 +1,15 @@
 package com.mehdi.oauth.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.ECDSAKeyProvider;
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.ECDSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -7,12 +17,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import java.security.PrivateKey;
+import java.security.interfaces.ECPrivateKey;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import static com.mehdi.oauth.utils.KeyUtils.loadPrivateKey;
 import static org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver.clientRegistrationId;
-
 
 @RestController
 public class AppController {
@@ -23,8 +36,43 @@ public class AppController {
         this.restClient = restClient;
     }
 
-    @GetMapping("/public")
-    public String publicPage() {
+    @GetMapping("/music_api_token")
+    public String publicPage() throws Exception {
+        // Load private key
+        PrivateKey privateKey = loadPrivateKey("src/main/resources/private_pkcs8.key");
+
+        // Create a signer using the private key
+        JWSSigner signer = new ECDSASigner((ECPrivateKey) privateKey);
+
+        // header
+
+
+        // Prepare the JWT claims set
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .issuer("939d7de6-ae69-4e0e-a36c-ee9c92b7a5c5")
+                .subject("6a736625-71ba-4ab4-9dd7-ca674038c17c")
+                .issueTime(new Date())
+                .expirationTime(new Date(System.currentTimeMillis() + 1209600000)) // 14 days expiry
+                .build();
+
+
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.ES256)
+                .type(JOSEObjectType.JWT)  // Set the type to JWT
+                .keyID("2ddeb1cb39")      // Optional: specify a key ID (kid)
+                .build();
+        // Create the signed JWT
+        SignedJWT signedJWT = new SignedJWT(
+                header,
+                claimsSet
+        );
+
+        // Sign the JWT
+        signedJWT.sign(signer);
+
+        // Serialize the JWT to a compact form
+        String token = signedJWT.serialize();
+
+        System.out.println("Generated JWT: " + token);
         return "This is a public page";
     }
 
---
+++ src/main/java/com/mehdi/oauth/utils/KeyUtils.java
@@ -0,0 +1,40 @@
+package com.mehdi.oauth.utils;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class KeyUtils {
+
+
+    public static PrivateKey loadPrivateKey(String filePath) throws Exception {
+        String key = Files.readString(Path.of(filePath))
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
+        byte[] keyBytes = Base64.getDecoder().decode(key);
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+
+    public static PublicKey loadPublicKey(String filePath) throws Exception {
+        String key = Files.readString(Path.of(filePath))
+                .replace("-----BEGIN PUBLIC KEY-----", "")
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s", "");
+        byte[] keyBytes = Base64.getDecoder().decode(key);
+
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        return keyFactory.generatePublic(keySpec);
+    }
+
+}
